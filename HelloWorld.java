7a499dac99af44b29b34fdceb3e43f5640003345
nsadawi <n.sadawi@gmail.com>
2016-04-27 14:12:05 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String args[]){
-        System.out.println("Hello World .. Jenkins is here!");
+        //System.out.println("Hello World .. Jenkins is here!");
+        System.out.println("Noureddin Sadawi");
     }
 }
